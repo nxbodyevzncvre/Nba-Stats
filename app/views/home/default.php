e34c0d02a859adbc1ea3dda3fe881c28adf73bd8
Danil <danil.ignashov@student.ukf.sk>
2025-03-06 12:29:18 +0100

    switched to vanilla css/ on a way to end up with ladnding

--- app/views/home/default.php
+++ app/views/home/default.php
@@ -5,57 +5,244 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href='https://fonts.googleapis.com/css?family=Big Shoulders Display' rel='stylesheet'>
-    <link rel="stylesheet" href="../../../public/css/output.css">
-    <style>
-        body {
-            font-family: 'Big Shoulders Display';
-        }
-        .bg-custom {
-            background-image: url('../../../public/images/background-image.png');
-        }
-    </style>
-
+    <link rel="stylesheet" href="/fin_proj/public/css/styles.css">
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
 </head>
-<body class="relative text-base">
-    <div class="relative min-h-screen">
-        <div class="absolute top-0 left-0 w-full h-full">
-            <video class="w-full h-full object-cover" autoplay muted loop playsinline>
-                <source src="../../../public/videos/background.mp4" type="video/mp4">
+<body>
+    <div class="default-hero">
+        <div class="default-hero__video">
+            <video autoplay muted loop playsinline>
+                <source src="/fin_proj/public/videos/background.mp4" type="video/mp4">
                 Your browser does not support the video tag.
             </video>
-            <div class="absolute top-0 left-0 w-full h-full bg-black/60"></div>
+            <div class="default-hero__overlay"></div>
         </div>
         
-        <div class="relative z-20">
-            <?php 
-            include "../../views/layout/header-default.php";
-            ?>
+        <div class="default-hero__header">
+            <?php include "../../views/layout/header-default.php"; ?>
         </div>
         
-        <div class="absolute inset-0 z-10 flex items-center justify-center px-4">
-            <div class="w-[900px] bg-black/30 p-8 rounded-lg text-white flex flex-col items-center">
-                <div class="flex justify-between items-center w-full mb-8">
-                    <div>
-                        <h1 class="text-[64px] font-bold mb-6 text-center">NBA STATS</h1>
-                        <p class="text-[32px] font-light leading-relaxed">Your Best Way to Dive Into<br>Basketball Brilliance</p>
+        <div class="default-hero__content">
+            <div class="default-hero__box">
+                <div class="default-hero__header-content">
+                    <div class="default-hero__text">
+                        <h1 class="default-hero__title">NBA STATS</h1>
+                        <p class="default-hero__description">Your Best Way to Dive Into<br>Basketball Brilliance</p>
                     </div>
-                    <div class="">
-                        <img src="../../../public/images/nba-teams.png" alt="NBA Teams" class="w-full">
+                    <div class="default-hero__image">
+                        <img src="/fin_proj/public/images/nba-teams.png" alt="NBA Teams">
                     </div>
                 </div>
-                <button class="px-10 py-3 bg-blue-600 hover:bg-blue-700 rounded-md text-[24px] transition-colors">Dive In</button>
+                <button class="btn btn--primary">Dive In</button>
             </div>
         </div>
     </div>
     
-    <main class="relative min-h-screen bg-custom bg-fixed bg-cover bg-center">
-        <div class="container-fluid mx-auto text-white">
-            <div class="text-4xl float-end p-4">
-                <p>YOUR <span class="text-red-700">BASKETBALL</span> JOURNEY STARTS HERE</p>
+    <main class="default-main">
+        <div class="container">
+            <div class="default-main__title">
+                <p>YOUR <span class="default-main__highlight">BASKETBALL</span> JOURNEY STARTS HERE</p>
             </div>
-            <div></div>
-            <div></div>
+            <section class="last-nba-news ">
+                <div class="swiper-container right-swiper">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <div class="swiper-slide__image"></div>
+                            <h3 class="swiper-slide__title">Lorem</h3>
+                            <p class="swiper-slide__description">descr about akwdk kiaia awdk aoaoao</p>
+                            <div class="swiper-slide__footer">
+                                <span>Posted: 05/06/24</span>
+                                <a href="#">Read more</a>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="swiper-slide__image"></div>
+                            <h3 class="swiper-slide__title">Lorem</h3>
+                            <p class="swiper-slide__description">descr about akwdk kiaia awdk aoaoao</p>
+                            <div class="swiper-slide__footer">
+                                <span>Posted: 05/06/24</span>
+                                <a href="#">Read more</a>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="swiper-slide__image"></div>
+                            <h3 class="swiper-slide__title">Lorem</h3>
+                            <p class="swiper-slide__description">descr about akwdk kiaia awdk aoaoao</p>
+                            <div class="swiper-slide__footer">
+                                <span>Posted: 05/06/24</span>
+                                <a href="#">Read more</a>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="swiper-slide__image"></div>
+                            <h3 class="swiper-slide__title">Lorem</h3>
+                            <p class="swiper-slide__description">descr about akwdk kiaia awdk aoaoao</p>
+                            <div class="swiper-slide__footer">
+                                <span>Posted: 05/06/24</span>
+                                <a href="#">Read more</a>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="swiper-slide__image"></div>
+                            <h3 class="swiper-slide__title">Lorem</h3>
+                            <p class="swiper-slide__description">descr about akwdk kiaia awdk aoaoao</p>
+                            <div class="swiper-slide__footer">
+                                <span>Posted: 05/06/24</span>
+                                <a href="#">Read more</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="last-nba-news">
+                <div class="swiper-container left-swiper">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <div class="swiper-slide__image"></div>
+                            <h3 class="swiper-slide__title">Lorem</h3>
+                            <p class="swiper-slide__description">descr about akwdk kiaia awdk aoaoao</p>
+                            <div class="swiper-slide__footer">
+                                <span>Posted: 05/06/24</span>
+                                <a href="#">Read more</a>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="swiper-slide__image"></div>
+                            <h3 class="swiper-slide__title">Lorem</h3>
+                            <p class="swiper-slide__description">descr about akwdk kiaia awdk aoaoao</p>
+                            <div class="swiper-slide__footer">
+                                <span>Posted: 05/06/24</span>
+                                <a href="#">Read more</a>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="swiper-slide__image"></div>
+                            <h3 class="swiper-slide__title">Lorem</h3>
+                            <p class="swiper-slide__description">descr about akwdk kiaia awdk aoaoao</p>
+                            <div class="swiper-slide__footer">
+                                <span>Posted: 05/06/24</span>
+                                <a href="#">Read more</a>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="swiper-slide__image"></div>
+                            <h3 class="swiper-slide__title">Lorem</h3>
+                            <p class="swiper-slide__description">descr about akwdk kiaia awdk aoaoao</p>
+                            <div class="swiper-slide__footer">
+                                <span>Posted: 05/06/24</span>
+                                <a href="#">Read more</a>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="swiper-slide__image"></div>
+                            <h3 class="swiper-slide__title">Lorem</h3>
+                            <p class="swiper-slide__description">descr about akwdk kiaia awdk aoaoao</p>
+                            <div class="swiper-slide__footer">
+                                <span>Posted: 05/06/24</span>
+                                <a href="#">Read more</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
     </main>
+
+    <div class="under-main">
+        <div class="upper-container">
+            <h2>Top NBA Players</h2>
+            <p>Discover the NBA’s finest talents and their statistics</p>
+        </div>
+        <div class="bottom-container">
+        <div class="cards-container">
+            <div class="card">
+                <img src="../../../public/images/lebron-default.png" alt="LeBron James">
+                <div class="card-content">
+                    <h2>LeBron James</h2>
+                    <div class="stats">
+                        <div class="stat"><span class="label">PPG</span><span class="value"> 25.7</span></div>
+                        <div class="stat"><span class="label">RPG</span><span class="value"> 12</span></div>
+                        <div class="stat"><span class="label">APG</span><span class="value"> 8.1</span></div>
+                        <div class="stat"><span class="label">FG%</span><span class="value"> 52.3</span></div>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <img src="../../../public/images/lebron-default.png" alt="LeBron James">
+                <div class="card-content">
+                    <h2>LeBron James</h2>
+                    <div class="stats">
+                        <div class="stat"><span class="label">PPG</span><span class="value">25.7</span></div>
+                        <div class="stat"><span class="label">RPG</span><span class="value">12</span></div>
+                        <div class="stat"><span class="label">APG</span><span class="value">8.1</span></div>
+                        <div class="stat"><span class="label">FG%</span><span class="value">52.3</span></div>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <img src="../../../public/images/lebron-default.png" alt="LeBron James">
+                <div class="card-content">
+                    <h2>LeBron James</h2>
+                    <div class="stats">
+                        <div class="stat"><span class="label">PPG</span><span class="value">25.7</span></div>
+                        <div class="stat"><span class="label">RPG</span><span class="value">12</span></div>
+                        <div class="stat"><span class="label">APG</span><span class="value">8.1</span></div>
+                        <div class="stat"><span class="label">FG%</span><span class="value">52.3</span></div>
+                    </div>
+                </div>
+            </div>
+        </div>  
+        </div>
+    </div>
+
+
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+    <script>
+        var swiperRight = new Swiper('.right-swiper', {
+            slidesPerView: 1,
+            spaceBetween: 20,
+            direction: 'horizontal',
+            loop: true, 
+            autoplay: {
+                delay: 0,
+                disableOnInteraction: false,
+                reverseDirection: false,
+                pauseOnMouseEnter: true,
+            },
+            speed: 5000, 
+            loopAdditionalSlides: 1,
+            effect: 'slide',
+            breakpoints: {
+                640: { slidesPerView: 2 },
+                1024: { slidesPerView: 3 },
+            },
+        });
+
+        var swiperLeft = new Swiper('.left-swiper', {
+            slidesPerView: 1,
+            spaceBetween: 20,
+            direction: 'horizontal',
+            loop: true,
+            autoplay: {
+                delay: 0,
+                disableOnInteraction: false,
+                reverseDirection: true,
+                pauseOnMouseEnter: true,
+            },
+            speed: 5000,
+            loopAdditionalSlides: 1,
+            effect: 'slide',
+            breakpoints: {
+                640: { slidesPerView: 2 },
+                1024: { slidesPerView: 3 },
+            },
+        });
+
+        document.querySelector('.right-swiper').addEventListener('mouseover', () => swiperRight.autoplay.stop());
+        document.querySelector('.right-swiper').addEventListener('mouseout', () => swiperRight.autoplay.start());
+        document.querySelector('.left-swiper').addEventListener('mouseover', () => swiperLeft.autoplay.stop());
+        document.querySelector('.left-swiper').addEventListener('mouseout', () => swiperLeft.autoplay.start());
+    </script>
+    <script type="module" src=""></script>
 </body>
 </html>
--- app/views/layout/header-default.php
+++ app/views/layout/header-default.php
@@ -1,6 +1,6 @@
-<header class="bg-transparent text-white z-10">
-    <div class="container mx-auto flex justify-between items-center">
-        <img src="../../../public/images/nba-logo.png" alt="NBA Logo" class="h-24 w-auto">
-        <a href="" class="px-6 py-2 text-4xl hover:text-gray-300 transition-colors">Join Us</a>
+<header class="header">
+    <div class="container header__container">
+        <img src="../../../public/images/nba-logo.png" alt="NBA Logo" class="header__logo">
+        <a href="" class="header__link">Join Us</a>
     </div>
 </header>
